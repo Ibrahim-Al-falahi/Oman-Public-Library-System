d67f248d392211a1434cfe6c196e635001791c0e
HadeelNajeeb <Hadilnajeeb@hotmail.com>
2025-05-08 14:31:12 +0400

    testadd obj for member and book

--- Oman Public Library System/Program.cs
+++ Oman Public Library System/Program.cs
@@ -1,10 +1,1 @@
-﻿namespace Oman_Public_Library_System
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿
