eb209b613907c36b119458344d4e8f60fb6dc1d6
ahmed <ahmed@AHMEDPC>
2025-05-08 13:40:10 +0400

    In class Library we added " Add method "

--- Oman Public Library System/Model/Library.cs
+++ Oman Public Library System/Model/Library.cs
@@ -11,11 +11,22 @@ namespace Oman_Public_Library_System.Model
 
         public int Id { get; set; }
         public string Name { get; set; }
+
+        List<Book> books;
         public Library(int id, string name)
         {
             Id = id;
             Name = name;
+            books = new List<Book>();
         }
+
+        
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+
+
 
     }
 }
